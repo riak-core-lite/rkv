3275aa1d22e7fc27fc4e73bd934d9d83cdeb74da
Mariano Guerra <mariano@marianoguerra.org>
2021-11-01 12:16:33 +0100

    Add shell.nix for reproducible dev environment

---
+++ shell.nix
@@ -0,0 +1,14 @@
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/e85975942742a3728226ac22a3415f2355bfc897.tar.gz") {} }:
+
+pkgs.mkShell {
+	LOCALE_ARCHIVE_2_27 = "${pkgs.glibcLocales}/lib/locale/locale-archive";
+	buildInputs = [
+		pkgs.glibcLocales
+		pkgs.erlang
+		pkgs.rebar3
+		pkgs.elixir
+	];
+	shellHook = ''
+		export LC_ALL=en_US.UTF-8
+	'';
+}
