ae5e5dc11b926f5c82db3bd8addc93305cb7894e
Mariano Guerra <mariano@marianoguerra.org>
2021-11-01 14:38:51 +0100

    handle mac os locale error

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 { pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/e85975942742a3728226ac22a3415f2355bfc897.tar.gz") {} }:
 
 pkgs.mkShell {
-	LOCALE_ARCHIVE_2_27 = "${pkgs.glibcLocales}/lib/locale/locale-archive";
+	LOCALE_ARCHIVE_2_27 = if (pkgs.glibcLocales != null) then "${pkgs.glibcLocales}/lib/locale/locale-archive" else "";
 	buildInputs = [
 		pkgs.glibcLocales
 		pkgs.erlang
